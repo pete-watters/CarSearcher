6356b99d970caa59fdd68a593da78db5cdb7d603
Peter Watters <peterwatters@MacBook-Pro-2.local>
2017-02-20 13:13:20 +0000

    Get mirage up and running

--- CarSearcher/mirage/config.js
+++ CarSearcher/mirage/config.js
@@ -1,26 +1,27 @@
 export default function() {
 
-  // These comments are here to help you get started. Feel free to delete them.
+  this.get('cars');
+  this.get('cars/:id');
 
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
-
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    http://www.ember-cli-mirage.com/docs/v0.2.x/shorthands/
-  */
+  //single data
+//   this.get('/cars/:id', function(db, request) {
+//     let id = request.params.id;
+//
+//     return {
+//       data: {
+//         type: 'cars',
+//         id: id,
+//         attributes: db.cars.find(id)
+//       }
+//     };
+//   });
+//
+// //collection of data
+//   this.get('/cars', function(db) {
+//     return {
+//       data: db.cars.map(attrs => (
+//       {type: 'cars', id: attrs.id, attributes: attrs }
+//       ))
+//     };
+//   });
 }

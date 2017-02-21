5e1b74649d83cf4105b083b2f1362507eeebcf0f
Peter Watters <peterwatters@MacBook-Pro-2.local>
2017-02-21 15:10:36 +0000

    Update Readme

--- CarSearcher/app/routes/application.js
+++ CarSearcher/app/routes/application.js
@@ -2,13 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-
-    console.log('sort cars by price');
-
     return Ember.RSVP.hash({
       rentaldetails: this.store.findAll('rentaldetail' , { reload: true }),
       rentalcompanys: this.store.findAll('rentalcompany', { reload: true }),
       cars: this.store.findAll('car', { reload: true }).then(function(cars){
+        // automatically sort the cars by price
         return cars.sortBy('totalChargeRateTotalAmount');
       })
     });

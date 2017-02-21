fc86ac856e2c222b803e5da8ff63589b266e6642
Peter Watters <peterwatters@MacBook-Pro-2.local>
2017-02-21 11:11:02 +0000

    Show full car detail when selected

--- CarSearcher/app/models/car.js
+++ CarSearcher/app/models/car.js
@@ -1,6 +1,20 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  status: DS.attr('string'),
+  vendorCode: DS.attr('string'),
   vendorName: DS.attr('string'),
-  vendorCode: DS.attr('string')
+  totalChargeCurrencyCode: DS.attr('string'),
+  totalChargeEstimatedTotalAmount: DS.attr('string'),
+  totalChargeRateTotalAmount: DS.attr('string'),
+  vehicleSpecAirConditionInd: DS.attr('string'),
+  vehicleSpecCode: DS.attr('string'),
+  vehicleSpecCodeContext: DS.attr('string'),
+  vehicleSpecDoorCount: DS.attr('string'),
+  vehicleSpecDriveType: DS.attr('string'),
+  vehicleSpecFuelType: DS.attr('string'),
+  vehicleSpecPassengerQuantity: DS.attr('string'),
+  vehicleSpecTransmissionType: DS.attr('string'),
+  vehicleSpecPictureURL: DS.attr('string'),
+  vehicleSpecDescription: DS.attr('string')
 });
